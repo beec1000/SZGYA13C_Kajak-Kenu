fb7647a4fbf26cb31cfd3fde19209e32eb6e14d8
beec1000 <beec1000.minecraft@gmail.com>
2025-01-30 11:30:43 +0100

    u

--- SZGYA13C_Kajak-Kenu/MainWindow.xaml.cs
+++ SZGYA13C_Kajak-Kenu/MainWindow.xaml.cs
@@ -8,19 +8,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SZGYA13C_Kajak_Kenu
 {
     public partial class MainWindow : Window
     {
         List<Kolcsonzes> kolcsonzes = new List<Kolcsonzes>();
-        public MainWindow()
-        {
+        public MainWindow() 
+        { 
             InitializeComponent();
 
             kolcsonzes = Kolcsonzes.Fromfile(@"..\..\..\src\kolcsonzes.txt");
+            OsszDG.ItemsSource = kolcsonzes;
 
-            test.Content = $"{kolcsonzes.Count}";
+            var elvitelIdo = kolcsonzes.Select(o => $"{o.ElvitelOra}:{o.ElvitelPerc}");
+            idoCB.ItemsSource = elvitelIdo;
+
+        }
+
+        public void HajoVizen(int Ora, int Perc)
+        {
+            int jelenIdoOra = DateTime.Now.Hour;
+            int jelenIdoPerc = DateTime.Now.Minute;
+
+            var vizen = kolcsonzes.Where(v => v).ToList();
+        }
+
+        private void idoCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var k = idoCB.SelectedItem.ToString().Split(":");
+            int keresettOra = int.Parse(k[0]);
+            int keresettPerc = int.Parse(k[1]);
+            HajoVizen(keresettOra, keresettPerc);
         }
     }
 }
